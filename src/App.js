76d1013ecc15ce7d06b40e2811738e60d884646b
Aschalew Gashaw <aschumay03.ga@gmail.com>
2022-10-02 02:50:43 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import {BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Footer from "./Components/Footer/Footer.js";
+import Header from "./Components/Header/Header.js";
+import Mainsection from "./Components/Mainsection/Mainsection.js";
+// import Twomainsection from "./Components/Mainsection/Twomainsection/Twomainsection.js";
+// import IntroInfo from "./Components/Mainsection/IntroInfo.js";
+import Mac from "./Components/applePages/Mac/Mac.js";
+import Four0Four from "./Components/applePages/Four0Four/Four0Four.js";
+import Iphone from "./Components/applePages/Iphone/Iphone.js";
+import ProductPage from "./Components/applePages/ProductPage/ProductPage.js";
+import Ipad from "./Components/applePages/Ipad/Ipad.js";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Mainsection />} />
+          <Route path="/Mac" element={<Mac />} />
+          <Route path="/Ipad" element={<Ipad />} />
+          <Route path="/Iphone" element={<Iphone />} />
+          <Route path="/Iphone/:product_id" element={<ProductPage />} />
+          <Route path="*" element={<Four0Four />} />
+        </Routes>
+        <Footer />
+      </>
+    </Router>
   );
 }
 
---
+++ src/Components/applePages/Four0Four/Four0Four.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Four0Four.css';
+
+const Four0Four = () => {
+  return (
+    <div>
+      <section className="internal-page-wrapper top-50 bottom-100">
+        <div className="container">
+          <div className="row h-100 align-items-center justify-content-center text-center">
+            <div className="col-12">
+              <h1>The page you’re looking for can’t be found.</h1>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Four0Four;
